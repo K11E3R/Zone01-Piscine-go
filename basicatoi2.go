5a67a2e31a4852d336ea05915b8490401b0bb8a8
Yassine Naanani <yasnaanani@noreply.zone01normandie.org>
2022-10-28 11:38:38 +0000

    Update 'basicatoi2.go'

--- basicatoi2.go
+++ basicatoi2.go
@@ -1,14 +1,38 @@
 package piscine
 
-
 func BasicAtoi2(s string) int {
-	res := 1
-	pow := len(s)-1
-	for i:=0; i<len(s);i++ {
-		if(!('0'<=s[i]  && s[i] <= '9')){
-			return 0
+	r := 0
+	d := 1
+	if s[0] == '-' {
+		for i := len(s) - 1; i >= 0; i-- {
+			if s[i] > '9' || s[i] < '0' {
+				return 0
+			} else {
+
+				a := 0
+				for j := s[i]; j > '0'; j-- {
+					a++
+				}
+				r = r + a*d
+				d = d * 10
+			}
 		}
-		res += (s[i]*((pow--)*10))
+		r = -r
 	}
-	return res;	
+	if s[0] == '+' {
+		for i := len(s) - 1; i > 0; i-- {
+			if s[i] > '9' || s[i] < '0' {
+				return 0
+			} else {
+				a := 0
+				for j := s[i]; j > '0'; j-- {
+					a++
+				}
+				r = r + a*d
+				d = d * 10
+
+			}
+		}
+	}
+	return r
 }

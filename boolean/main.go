93715bc675c65d28fea4320b7e3652f28a2c6d70
Yassine Naanani <yasnaanani@noreply.zone01normandie.org>
2022-11-07 14:55:47 +0000

    Update 'boolean/main.go'

--- boolean/main.go
+++ boolean/main.go
@@ -2,34 +2,22 @@ package main
 
 import (
 	"os"
-	"errors"
-	"unicode/utf8"
+
+	"github.com/01-edu/z01"
 )
-func PrintRune(r rune) error {
-	l := utf8.RuneLen(r)
-	if l == -1 {
-		return errors.New("The rune is not a valid value to encode in UTF-8")
-	}
-	p := make([]byte, l)
-	utf8.EncodeRune(p, r)
-	_, err := os.Stdout.Write(p)
-	return err
-}
-
-
 
 func printStr(str string) {
 	arrayStr := []rune(str)
 
 	for i := 0; i < len(arrayStr); i++ {
-		PrintRune(arrayStr[i])
+		z01.PrintRune(arrayStr[i])
 	}
-	PrintRune('\n')
+	z01.PrintRune('\n')
 }
 
 func isEven() int {
 	lengthOfArg := os.Args[1:]
-	if len(lengthOfArg) % 2 == 0{
+	if len(lengthOfArg)%2 == 0 {
 		return 1
 	}
 	return 0

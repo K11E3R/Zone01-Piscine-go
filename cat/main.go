77fcfa51bb1429878a82a9a58ce54ffb4dea0160
Yassine Naanani <yasnaanani@noreply.zone01normandie.org>
2022-11-09 19:02:57 +0000

    Update 'cat/main.go'

--- cat/main.go
+++ cat/main.go
@@ -8,22 +8,15 @@ import (
 
 func main() {
 	size := len(os.Args)
-	/*	if size == 1 {
-		for { //could use fmt.scanf but not sure if the student can use it
-			reader := bufio.NewReader(os.Stdin)
-			data, _ := reader.ReadString('\n')
-			fmt.Print(data)
+	if len(os.Args) > 1 {
+		for i := 1; i < size; i++ {
+			data, err := ioutil.ReadFile(os.Args[i])
+			if err != nil {
+				fmt.Println(err.Error())
+				return
+			}
+			fmt.Println(string(data))
+			fmt.Print()
 		}
-	} else if len(os.Args) > 1 {*/
-	for i := 1; i < size; i++ {
-		data, err := ioutil.ReadFile(os.Args[i])
-		if err != nil {
-			fmt.Println(err.Error())
-			return
-		}
-		fmt.Println(string(data))
-		fmt.Println()
 	}
-
-	//}
 }

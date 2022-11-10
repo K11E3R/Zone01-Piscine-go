1535c0e1e9cde5b6e83d720b5cd6a7ae90e3e201
Yassine Naanani <yasnaanani@noreply.zone01normandie.org>
2022-11-10 21:52:07 +0000

    Update 'compact.go'

--- compact.go
+++ compact.go
@@ -1,8 +1,8 @@
 package piscine
 
-func Compact(ptr *[]string, length int) int {
+func Compact(ptr *[]string) int {
 	count := 0
-	for i := 0; i < length; i++ {
+	for i := 0; i < len(string); i++ {
 		if (*ptr)[i] != " " {
 			count++
 		}

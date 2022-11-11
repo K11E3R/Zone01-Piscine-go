5392415a983fb1fd687e27232ec57e4904607df1
Yassine Naanani <yasnaanani@noreply.zone01normandie.org>
2022-11-11 01:19:05 +0000

    Update 'max.go'

--- max.go
+++ max.go
@@ -1,11 +1,11 @@
-package main 
+package main
 
 func Max(a []int) int {
-    maxx := a[0]
-    for _,a := range(a){
-        if a > maxx {
-            maxx = a
-        }
-    }
-    return maxx
+	maxx := a[0]
+	for _, a := range a {
+		if a > maxx {
+			maxx = a
+		}
+	}
+	return maxx
 }

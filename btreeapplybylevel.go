2203e7cb9b8c7fd018fc894dbe94dfa31190a4ba
Yassine Naanani <yasnaanani@noreply.zone01normandie.org>
2022-11-15 16:28:32 +0000

    Update 'btreeapplybylevel.go'

--- btreeapplybylevel.go
+++ btreeapplybylevel.go
@@ -1,25 +1,13 @@
 package piscine
 
-import (
-	"github.com/01-edu/z01"
-)
-
-func BTreeApplyByLevel(root *TreeNode, fn interface{}) {
-	novo := BTreeLevelCount(root)
-	for d := 1; d <= novo; d++ {
-		PrintNodesLevel(root, d, fn)
+func BTreeApplyToGivenLevel(root *TreeNode, level int, f func(...interface{}) (int, error)) {
+	if root == nil {
+		return
 	}
-}
-
-func PrintNodesLevel(root *TreeNode, level int, fn interface{}) {
-	if root != nil {
-		if level == 1 {
-			ar := []interface{}{root.Data}
-			z01.Call(fn, ar)
-
-		} else if level > 1 {
-			PrintNodesLevel(root.Left, level-1, fn)
-			PrintNodesLevel(root.Right, level-1, fn)
-		}
+	if level == 1 {
+		f(root.Data)
+	} else if level > 1 {
+		BTreeApplyToGivenLevel(root.Left, level-1, f)
+		BTreeApplyToGivenLevel(root.Right, level-1, f)
 	}
 }

287b34ae804bd2033e457ec70d7b343c563c7c21
Yassine Naanani <yasnaanani@noreply.zone01normandie.org>
2022-11-16 14:42:34 +0000

    Update 'btreeapplybylevel.go'

--- btreeapplybylevel.go
+++ btreeapplybylevel.go
@@ -1,32 +1,32 @@
 package piscine
 
 func BTreeApplyByLevel(root *TreeNode, f func(...interface{}) (int, error)) {
-	height := BTreeLevelCount(root)
+	height := cc(root)
 	for level := 1; level <= height; level++ {
-		BTreeApplyToGivenLevel(root, level, f)
+		ttt(root, level, f)
 	}
 }
 
-func BTreeApplyToGivenLevel(root *TreeNode, level int, f func(...interface{}) (int, error)) {
+func ttt(root *TreeNode, level int, f func(...interface{}) (int, error)) {
 	if root == nil {
 		return
 	}
 	if level == 1 {
 		f(root.Data)
 	} else if level > 1 {
-		BTreeApplyToGivenLevel(root.Left, level-1, f)
-		BTreeApplyToGivenLevel(root.Right, level-1, f)
+		ttt(root.Left, level-1, f)
+		ttt(root.Right, level-1, f)
 	}
 }
 
-func BTreeLevelCount(root *TreeNode) int {
+func cc(root *TreeNode) int {
 	countl := 0
 	countr := 0
 
 	if root != nil {
-		countl = BTreeLevelCount(root.Left)
+		countl = cc(root.Left)
 		countl++
-		countr = BTreeLevelCount(root.Right)
+		countr = cc(root.Right)
 		countr++
 	}
 	if countl > countr {
